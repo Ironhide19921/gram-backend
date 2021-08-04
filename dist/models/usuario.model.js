9e04d1c84f2c7fa5b504194cc333c8aa49d11c78
Gerardo <gera_lps@hotmail.com.ar>
2021-08-04 15:52:40 -0300

    bcrypt borrado

--- dist/models/usuario.model.js
+++ dist/models/usuario.model.js
@@ -1,36 +1,39 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 var mongoose_1 = require("mongoose");
-var bcrypt_1 = __importDefault(require("bcrypt"));
+var bcrypt_1 = __importDefault(require("bcryptjs"));
 var usuarioSchema = new mongoose_1.Schema({
-    nombre: {
-        type: String,
-        required: [true, 'El nombre es necesario']
-    },
-    avatar: {
-        type: String,
-        default: 'av-1.png'
-    },
-    email: {
-        type: String,
-        unique: true,
-        required: [true, 'El correo es necesaria']
-    },
-    password: {
-        type: String,
-        required: [true, 'La contraseña es necesaria']
-    }
+  nombre: {
+    type: String,
+    required: [true, "El nombre es necesario"],
+  },
+  avatar: {
+    type: String,
+    default: "av-1.png",
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, "El correo es necesaria"],
+  },
+  password: {
+    type: String,
+    required: [true, "La contraseña es necesaria"],
+  },
 });
-usuarioSchema.method('compararPassword', function (password) {
-    if (password === void 0) { password = ''; }
-    if (bcrypt_1.default.compareSync(password, this.password)) {
-        return true;
-    }
-    else {
-        return false;
-    }
+usuarioSchema.method("compararPassword", function (password) {
+  if (password === void 0) {
+    password = "";
+  }
+  if (bcrypt_1.default.compareSync(password, this.password)) {
+    return true;
+  } else {
+    return false;
+  }
 });
-exports.Usuario = mongoose_1.model('Usuario', usuarioSchema);
+exports.Usuario = mongoose_1.model("Usuario", usuarioSchema);
